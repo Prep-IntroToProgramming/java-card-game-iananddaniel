df83d9e388e5d081c3d4a721ef350ce31e3c57a1
Dr. Frewen <sfrewen@flintridgeprep.org>
2018-10-09 09:35:18 -0700

    Updated indentation

--- VehicleTestDrive.java
+++ VehicleTestDrive.java
@@ -1,23 +1,23 @@
 class VehicleTestDrive {  
-  public static void main(String args[]) {  
-    Vehicle minivan = new Vehicle();  
-    int range;  
 
+    public static void main(String args[]) { 
 
-    // assign values to fields in minivan 
+        Vehicle minivan = new Vehicle();  
+        int range;  
 
-    minivan.passengers = 7; 
-    minivan.fuelcap = 16; 
-    minivan.mpg = 21; 
-  
-    // compute the range assuming a full tank of gas 
+        // assign values to fields in minivan 
+        minivan.passengers = 7; 
+        minivan.fuelcap = 16; 
+        minivan.mpg = 21; 
 
-    range = minivan.fuelcap * minivan.mpg; 
+        // compute the range assuming a full tank of gas 
 
-    System.out.println("Minivan can carry " + minivan.passengers + 
+        range = minivan.fuelcap * minivan.mpg; 
 
-                       " with a range of " + range);  
+        System.out.println("Minivan can carry " + minivan.passengers + 
 
-  }  
+            " with a range of " + range);  
+
+    }  
 
 }
